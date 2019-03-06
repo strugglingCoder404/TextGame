a41afb5432f011016a2b62ce9476136f2ce995f9
sebas <strugglingcoder404@gmail.com>
2019-03-06 17:03:35 +0100

    test

--- Text Fighting Game.cpp
+++ Text Fighting Game.cpp
@@ -9,9 +9,7 @@ class Enemy
 	public:
 		std::string element;
 		int hp = 50;
-
-	private:
-		int atk = 15;
+		int atk = 17;
 		int critAtk = 20;	
 };
 
@@ -28,8 +26,8 @@ class Player
 	public:
 		std::string element;
 		int hp = 150;
-		int atk = 20;
-		int critAtk = 30;
+		int atk = 15;
+		int critAtk = 25;
 };
 
 int main()
@@ -46,10 +44,12 @@ int main()
 	std::string Ice = "Ice";
 	char decision;
 	std::string fightDecision;
-	
+	int uselessVariable;
+
 
 	Enemy enemy1;
-	enemy1.hp = 100; 
+	enemy1.hp = 120;
+	enemy1.critAtk;
 
 
 
@@ -66,45 +66,46 @@ int main()
 		std::cin >> decision;
 
 		if (decision == 'y') {
-			std::cout << "'Thank you so much! Go to the high volcano please. There will be your first enemy!'\n"; Sleep(1500);
+			std::cout << "'Thank you so much! Go to the high volcano please. There will be your first enemy!'\n"; Sleep(1000);
 			std::cout << "."; Sleep(500); std::cout << "."; Sleep(500); std::cout << "."; Sleep(500);
-/*you*/		std::cout << "Y"; Sleep(50);std::cout << "o"; Sleep(50); std::cout << "u"; Sleep(50); std::cout << " "; Sleep(50);
-/*already*/	std::cout << "a"; Sleep(50); std::cout << "l"; Sleep(50); std::cout << "r"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << "a"; Sleep(50); std::cout << "d"; Sleep(50); std::cout << "y"; Sleep(50);std::cout << " "; Sleep(50);
-/*see*/		std::cout << "s"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
-/*the*/		std::cout << "t"; Sleep(50); std::cout << "h"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
-/*volcano*/	std::cout << "v"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "l"; Sleep(50); std::cout << "c"; Sleep(50); std::cout << "a"; Sleep(50); std::cout << "n"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << " "; Sleep(50);
-/*on*/		std::cout << "o"; Sleep(50); std::cout << "n"; Sleep(50); std::cout << " "; Sleep(50);
-/*the*/	    std::cout << "t"; Sleep(50); std::cout << "h"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
-/*horizon*/ std::cout << "h"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "r"; Sleep(50); std::cout << "i"; Sleep(50); std::cout << "z"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "n"; Sleep(50); 
+			/*you*/		std::cout << "Y"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "u"; Sleep(50); std::cout << " "; Sleep(50);
+			/*already*/	std::cout << "a"; Sleep(50); std::cout << "l"; Sleep(50); std::cout << "r"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << "a"; Sleep(50); std::cout << "d"; Sleep(50); std::cout << "y"; Sleep(50); std::cout << " "; Sleep(50);
+			/*see*/		std::cout << "s"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
+			/*the*/		std::cout << "t"; Sleep(50); std::cout << "h"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
+			/*volcano*/	std::cout << "v"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "l"; Sleep(50); std::cout << "c"; Sleep(50); std::cout << "a"; Sleep(50); std::cout << "n"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << " "; Sleep(50);
+			/*on*/		std::cout << "o"; Sleep(50); std::cout << "n"; Sleep(50); std::cout << " "; Sleep(50);
+			/*the*/	    std::cout << "t"; Sleep(50); std::cout << "h"; Sleep(50); std::cout << "e"; Sleep(50); std::cout << " "; Sleep(50);
+			/*horizon*/ std::cout << "h"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "r"; Sleep(50); std::cout << "i"; Sleep(50); std::cout << "z"; Sleep(50); std::cout << "o"; Sleep(50); std::cout << "n"; Sleep(50);
 			std::cout << "."; Sleep(500); std::cout << "."; Sleep(500); std::cout << ".\n"; Sleep(500);
 			std::cout << "'What is that?!'";
-			std::cout << "'It looks like one of those monsters the villagers told me about "; 
+			std::cout << "'It looks like one of those monsters the villagers told me about ";
 			std::cout << "."; Sleep(300); std::cout << "."; Sleep(300); std::cout << "."; Sleep(300);
 			std::cout << " and it burns!";
 			std::cout << "\n\nThe fight begins!\nTo attack type 'atk' and to heal yourself type 'heal'!";
 			//starting the battle
 			while (enemy1.hp > 0 && player.hp > 0)
 			{
-				std::cout << "Do you want to attack or heal?\n";
+				std::cout << "\nDo you want to attack or heal?\n";
 				std::cin >> fightDecision;
 				if (player.element == water | player.element == Water)
 				{
-					if (fightDecision == "atk") 
+					if (fightDecision == "atk")
 					{
 						enemy1.hp = enemy1.hp - player.critAtk;
+						std::cout << "\nNice work! You did " << player.critAtk << " damage!";
 					}
 					else if (fightDecision == "heal")
 					{
-						if (player.hp == 100)
+						if (player.hp == 150)
 						{
-							std::cout << "You already have full hp!";
+							std::cout << "\n\nYou already have full hp!\n";
 						}
 						else
 						{
 							player.hp = player.hp + 15;
-							std::cout << "You have " << player.hp << " health points!";
+							std::cout << "\nYou now have " << player.hp << " health points!";
 						}
-						
+
 					}
 				}
 				else
@@ -112,53 +113,90 @@ int main()
 					if (fightDecision == "atk")
 					{
 						enemy1.hp = enemy1.hp - player.atk;
-						
+
 					}
 					else if (fightDecision == "heal")
 					{
-						if (player.hp == 100)
+						if (player.hp == 150)
 						{
-							std::cout << "You already have full hp!";
+							std::cout << "\nYou already have full hp!\n";
 						}
 						else
 						{
 							player.hp = player.hp + 15;
-							std::cout << "You have " << player.hp << " health points!";
+							std::cout << "\nYou now have " << player.hp << " health points!";
 						}
 
 					}
 				}
-				if (enemy1.hp > 0)
+				if (enemy1.hp > 0 && fightDecision != "heal")
 				{
-					std::cout << "The enemy has " << enemy1.hp << " hp!";
+					std::cout << "\nThe enemy now has " << enemy1.hp << " hp!";
 				}
 				else if (enemy1.hp <= 0)
 				{
-					std::cout << "The enemy is dead!\n";
+					std::cout << "\nThe enemy is dead!\n";
 				}
-			}
-			if (enemy1.hp <= 0)
-			{
-				std::cout << "You won! Nice job!";
-			}
-			else if (player.hp <= 0)
-			{
-				std::cout << "You died!Try again the next time!";
+
+				Sleep(200);
+				if (player.hp > 0 && enemy1.hp > 0)
+				{
+					std::cout << "\The enemy now attacks!";
+				}
+				if (player.element == ice | player.element == Ice && player.hp > 0 && enemy1.hp > 0)
+				{
+					std::cout << "\nThe enemy did critical damage!";
+					player.hp = player.hp - enemy1.critAtk;
+				}
+				else if(player.element != ice | player.element != Ice && player.hp > 0 && enemy1.hp > 0)
+				{
+					std::cout << "\nThe enemy did some damage!";
+					player.hp = player.hp - enemy1.atk;
+				}
+
+				if (player.hp > 0 && enemy1.hp > 0)
+				{
+					std::cout << "\nYou now have " << player.hp << " health points.";
+				}
+
+				if (enemy1.hp <= 0)
+				{
+					std::cout << "\n\nYou won! Nice job!\n";
+					std::cout << "You go away from the volcano damaged, but alive. You need to find a place to heal!\n";
+					std::cout << "You have heard about the mages which live a few miles away from the volcano, which are said to be dangerous, but have powerful healing abilities.\n";
+					std::cout << "The oter possibility is to go into the forest and go search some herbs yourself. \nWhat do you do?('1' or '2')";
+					std::cin >> uselessVariable;
+					if (uselessVariable == 1)
+					{
+						std::cout << "Good luck on your way to them. But hurry up before you starve or die of thirst!";
+					}
+					else if (uselessVariable == 2)
+					{
+						std::cout << "Let's hope that you will find some herbs. But hurry up before you starve or die of thirst!";
+					}
+				}
+				else if (player.hp <= 0)
+				{
+					std::cout << "\n\nYou died!Try again the next time!\n";
+					std::cin.get();
+				}
+
 			}
 		}
-		else if(decision == 'n'){
+
+
+		else if (decision == 'n')
+		{
 			std::cout << "'Get fucking lost then you stupid cunt!'";
 			std::cin.get();
 		}
-		else {
-			std::cout << "Your input was invalid. Please restart the program and try again.";
-			std::cin.get();
-		}
+
+
 	}
-	else {
+	else if (player.element != fire | player.element != Fire | player.element != water | player.element != Water | player.element != ice | player.element != Ice)
+		{
 		std::cout << "Your input was invalid. Please restart the program and try again.";
 		std::cin.get();
-	}
-std::cin.get();
+		}
+	std::cin.get();
 }
-
